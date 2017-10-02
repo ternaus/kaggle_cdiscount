70abf37f8854a19ed14240d432b6344d6239ea2a
Vladimir Iglovikov <iglovikov@gmail.com>
2017-10-02 15:43:47 -0700

    Added script that maps train.bson to images

---
+++ src/bson_2_jpg.py
@@ -0,0 +1,61 @@
+import bson
+import pandas as pd
+from pathlib import Path
+
+
+import multiprocessing as mp
+
+prod_to_category = mp.Manager().dict()  # note the difference
+
+
+def process(q, iolock):
+    while True:
+        d = q.get()
+        if d is None:
+            break
+        p_id = d['_id']
+        c_id = d['category_id']
+        category_path = (base_path / str(c_id))
+
+        category_path.mkdir(exist_ok=True)
+
+        for e, pic in enumerate(d['imgs']):
+            fname = str(category_path / '{p_id}_{e}.jpg'.format(p_id=p_id, e=e))
+
+            with open(fname, 'wb') as f:
+                f.write(pic['picture'])
+
+
+data_path = Path('../data')
+base_path = data_path / 'train'
+base_path.mkdir(exist_ok=True)
+
+
+n_cores = 16
+prods = mp.Manager().dict()
+
+q = mp.Queue(maxsize=n_cores)
+iolock = mp.Lock()
+pool = mp.Pool(n_cores, initializer=process, initargs=(q, iolock))
+
+
+# process the file
+
+data = bson.decode_file_iter(open(str(data_path / 'train.bson'), 'rb'))
+for c, d in enumerate(data):
+    q.put(d)
+
+# tell workers we're done
+
+for _ in range(n_cores):
+    q.put(None)
+
+pool.close()
+pool.join()
+
+# convert back to normal dictionary
+prod_to_category = dict(prod_to_category)
+
+prod_to_category = pd.DataFrame.from_dict(prod_to_category, orient='index')
+prod_to_category.index.name = '_id'
+prod_to_category.rename(columns={0: 'category_id'}, inplace=True)
