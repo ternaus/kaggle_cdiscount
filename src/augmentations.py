05a81b2177e59cd45d0538c56dbb189984838a01
Vladimir Iglovikov <iglovikov@gmail.com>
2017-10-02 15:50:28 -0700

    Added scripts for training

---
+++ src/augmentations.py
@@ -0,0 +1,222 @@
+"""
+Extra augmentations for pytorch
+"""
+
+from imgaug import augmenters as iaa
+import numpy as np
+from PIL import Image
+import random
+import math
+import cv2
+import numbers
+
+
+class RandomHorizontalFlip(object):
+    """Randomly vertically flips the given PIL.Image with given probability
+    """
+    def __init__(self, probability):
+        self.probability = probability
+
+    def __call__(self, img):
+        img_array = np.array(img)
+        flipper = iaa.Fliplr(self.probability)
+        img_n = flipper.augment_image(img_array)
+
+        return Image.fromarray(img_n, mode='RGB')
+
+
+class Random90Rotation(object):
+    """Randomly rotates image on [0, 90, 180, 270] degrees
+    """
+
+    def __call__(self, img):
+        angles = [0, 90, 180, 270]
+        random_angle = np.random.choice(angles)
+
+        if random_angle == 0:
+            return img
+
+        img_array = np.array(img)
+
+        augmentor = iaa.Affine(rotate=random_angle)
+
+        img_n = augmentor.augment_image(img_array)
+
+        return Image.fromarray(img_n, mode='RGB')
+
+
+class D4(object):
+    """Random transformation from D4 group
+    """
+    def __call__(self, img):
+        if random.random() < 0.5:
+            img = np.transpose(img, [1, 0, 2])
+
+        if random.random() < 0.5:
+            img = np.flipud(img)
+
+        if random.random() < 0.5:
+            img = np.fliplr(img)
+
+        return np.copy(img)
+
+
+class GaussianBlur(object):
+    """Blurs image with blur in a range
+    """
+    def __init__(self, from_blur=0, to_blur=4):
+        self.from_blur = from_blur
+        self.to_blur = to_blur
+
+    def __call__(self, img):
+        img_array = np.array(img)
+
+        blurer = iaa.GaussianBlur(sigma=(self.from_blur, self.to_blur))
+
+        img_n = blurer.augment_image(img_array)
+
+        return Image.fromarray(img_n, mode='RGB')
+
+
+class Add(object):
+    """Adds values within a range
+
+    per_channel : bool, optional(default=False)
+            Whether to use the same value for all channels (False)
+            or to sample a new value for each channel (True).
+            If this value is a float p, then for p percent of all images
+            per_channel will be treated as True, otherwise as False.
+    """
+    def __init__(self, from_add=-10, to_add=+10, per_channel=0.5):
+        self.from_add = from_add
+        self.to_add = to_add
+        self.per_channel = per_channel
+
+    def __call__(self, img):
+        img_array = np.array(img)
+
+        adder = iaa.Add((self.from_add, self.to_add), per_channel=self.per_channel)
+
+        img_n = adder.augment_image(img_array)
+
+        return Image.fromarray(img_n, mode='RGB')
+
+
+class Rotate(object):
+    """Rotates image
+    """
+    def __init__(self, from_angle=-20, to_angle=+20, mode='reflect'):
+        self.from_angle = from_angle
+        self.to_angle = to_angle
+        self.mode = mode
+
+    def __call__(self, img):
+        img_array = np.array(img)
+
+        rotator = iaa.Affine(rotate=(self.from_angle, self.to_angle), mode=self.mode)
+        img_n = rotator.augment_image(img_array)
+
+        return Image.fromarray(img_n, mode='RGB')
+
+
+class ContrastNormalization(object):
+    """Changes contrast
+    """
+    def __init__(self, contrast_from=0.9, contrast_to=1.1, per_channel=0.5):
+        self.contrast_from = contrast_from
+        self.contrast_to = contrast_to
+        self.per_channel = per_channel
+
+    def __call__(self, img):
+        img_array = np.array(img)
+
+        contrastor = iaa.ContrastNormalization((self.contrast_from, self.contrast_to), self.per_channel)
+        img_n = contrastor.augment_image(img_array)
+
+        return Image.fromarray(img_n, mode='RGB')
+
+
+class CenterCrop(object):
+    """Crops the given np.array at the center to have a region of
+    the given size. size can be a tuple (target_height, target_width)
+    or an integer, in which case the target will be of a square shape (size, size)
+    """
+
+    def __init__(self, size):
+        if isinstance(size, numbers.Number):
+            self.size = (int(size), int(size))
+        else:
+            self.size = size
+
+    def __call__(self, img):
+        h, w = img.shape[0], img.shape[1]
+        # w, h = img.size
+        th, tw = self.size
+        x1 = int(round((w - tw) / 2.))
+        y1 = int(round((h - th) / 2.))
+        return img[y1:y1 + th, x1:x1 + th, :].astype(np.int64)
+
+
+class RandomCrop(object):
+    """Crops the given np.array randomly to have a region of
+    the given size. size can be a tuple (target_height, target_width)
+    or an integer, in which case the target will be of a square shape (size, size)
+    """
+
+    def __init__(self, size):
+        if isinstance(size, numbers.Number):
+            self.size = (int(size), int(size))
+        else:
+            self.size = size
+
+    def __call__(self, img):
+        h, w = img.shape[0], img.shape[1]
+        # w, h = img.size
+        th, tw = self.size
+        x1 = np.random.randint(0, w - tw - 1)
+        y1 = np.random.randint(0, h - th - 1)
+        return img[y1:y1 + th, x1:x1 + th, :].astype(np.int64)
+
+
+class RandomSizedCrop(object):
+    """Random crop the given np.array to a random size of (0.08 to 1.0) of the original size
+    and and a random aspect ratio of 3/4 to 4/3 of the original aspect ratio
+    This is popularly used to train the Inception networks
+    size: size of the smaller edge
+    interpolation: Default:  CV_INTER_LINEAR
+    """
+
+    def __init__(self, size, interpolation=cv2.INTER_LINEAR):
+        self.size = size
+        self.interpolation = interpolation
+
+    def __call__(self, img):
+        for attempt in range(10):
+            area = img.shape[0] * img.shape[1]
+            target_area = random.uniform(0.08, 1.0) * area
+            aspect_ratio = random.uniform(3. / 4, 4. / 3)
+
+            w = int(round(math.sqrt(target_area * aspect_ratio)))
+            h = int(round(math.sqrt(target_area / aspect_ratio)))
+
+            if random.random() < 0.5:
+                w, h = h, w
+
+            if w <= img.shape[1] and h <= img.shape[0]:
+                y1 = random.randint(0, img.shape[0] - h)
+                x1 = random.randint(0, img.shape[1] - w)
+
+                img = img[y1:y1+h, x1:x1+w, :]
+
+                assert img.shape[0] == h
+                assert img.shape[1] == w
+
+                return cv2.resize(img, (self.size, self.size), interpolation=self.interpolation).astype(np.int64)
+
+        # Fallback
+        scale = cv2.resize(img, (self.size, self.size), interpolation=self.interpolation)
+
+        crop = CenterCrop(self.size)
+        result = crop(scale)
+
+        return result.astype(np.int64)
--- src/train.py
+++ src/train.py
@@ -15,81 +15,46 @@ from torch.utils.data.dataset import Dataset
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from torch import nn
-from torch.autograd import Variable
-from torch.nn import MultiLabelSoftMarginLoss
 
-from torchvision.models import resnet50, resnet101, resnet152
-from torchvision.models import densenet121, densenet161, densenet201
+from torch.nn import CrossEntropyLoss
 
 import torch.nn.functional as F
 import utils
 import tqdm
 
-from sklearn.metrics import fbeta_score
 import numpy as np
-import shutil
 import argparse
 from torch.optim import Adam
 import data_loader
-import augmentations
 import models
-
-
-def f2_score(y_true, y_pred):
-    # fbeta_score throws a confusing error if inputs are not numpy arrays
-    y_true, y_pred, = np.array(y_true), np.array(y_pred)
-    # We need to use average='samples' here, any other average method will generate bogus results
-    return fbeta_score(y_true, y_pred, beta=2, average='samples')
+import augmenations
 
 
 def validation(model, criterion, valid_loader):
     model.eval()
     losses = []
-    f2_scores = []
+    accuracy_scores = []
     for inputs, targets in valid_loader:
         inputs = utils.variable(inputs, volatile=True)
         targets = utils.variable(targets)
         outputs = model(inputs)
         loss = criterion(outputs, targets)
         losses.append(loss.data[0])
-        f2_scores.append(f2_score(y_true=targets.data.cpu().numpy(), y_pred=F.sigmoid(outputs).data.cpu().numpy() > 0.2))
+        accuracy_scores.append(float(targets.data.cpu().numpy() == outputs.data.cpu().numpy()))
+
     valid_loss = np.mean(losses)  # type: float
-    valid_f2 = np.mean(f2_scores)  # type: float
-    print('Valid loss: {:.4f}, F2: {:.4f}'.format(valid_loss, valid_f2))
-    return {'valid_loss': valid_loss, 'valid_f2': valid_f2}
-
-
-def get_model(num_classes, model_type='resnet50'):
-    if model_type == 'resnet50':
-        model = resnet50(pretrained=True).cuda()
-        model.fc = nn.Linear(model.fc.in_features, num_classes).cuda()
-    elif model_type == 'resnet101':
-        model = resnet101(pretrained=True).cuda()
-        model.fc = nn.Linear(model.fc.in_features, num_classes).cuda()
-    elif model_type == 'resnet152':
-        model = resnet152(pretrained=True).cuda()
-        model.fc = nn.Linear(model.fc.in_features, num_classes).cuda()
-    elif model_type == 'densenet121':
-        model = densenet121(pretrained=True).cuda()
-        model.classifier = nn.Linear(model.classifier.in_features, num_classes).cuda()
-    elif model_type == 'densenet161':
-        model = densenet161(pretrained=True).cuda()
-        model.classifier = nn.Linear(model.classifier.in_features, num_classes).cuda()
-    elif model_type == 'densenet201':
-        model = densenet201(pretrained=True).cuda()
-        model.classifier = nn.Linear(model.classifier.in_features, num_classes).cuda()
-    return model
+    valid_accuracy = np.mean(accuracy_scores)  # type: float
+    print('Valid loss: {:.4f}, F2: {:.4f}'.format(valid_loss, valid_accuracy))
+    return {'valid_loss': valid_loss, 'accuracy': valid_accuracy}
 
 
 def add_args(parser):
     arg = parser.add_argument
-    arg('root', help='checkpoint root')
+    arg('--root', default='runs/debug', help='checkpoint root')
     arg('--batch-size', type=int, default=4)
     arg('--n-epochs', type=int, default=30)
     arg('--lr', type=float, default=0.0001)
     arg('--workers', type=int, default=12)
-    arg('--fold', type=int)
-    arg('--n-folds', type=int, default=10)
     arg('--clean', action='store_true')
     arg('--epoch-size', type=int)
     arg('--device-ids', type=str, help='For example 0,1 to run on two GPUs')
@@ -101,7 +66,6 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     arg = parser.add_argument
-    arg('--mode', choices=['train', 'valid', 'predict_valid', 'predict_test'], default='train')
     add_args(parser)
     args = parser.parse_args()
     model_name = args.model
@@ -109,24 +73,17 @@ if __name__ == '__main__':
     batch_size = args.batch_size
 
     train_transform = transforms.Compose([
-        transforms.RandomSizedCrop(224),
         augmentations.D4(),
-        # augmentations.Rotate(),
-        # augmentations.GaussianBlur(),
-        augmentations.Add(-5, 5, per_channel=True),
-        augmentations.ContrastNormalization(0.8, 1.2, per_channel=True),
+        # transforms.Scale(224),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
 
-    train_loader, valid_loader = data_loader.get_loaders(batch_size, train_transform=train_transform, fold=args.fold)
+    train_loader, valid_loader = data_loader.get_loaders(batch_size, args, train_transform=train_transform)
 
-    num_classes = 17
+    num_classes = 5270
 
-    # model = get_model(num_classes, model_name)
-
-    # model = getattr(models, args.model)(num_classes=num_classes)
-    model = get_model(num_classes, model_name)
+    model = models.ResNetFinetune(num_classes, net_cls=models.M.resnet50)
     model = utils.cuda(model)
 
     if utils.cuda_is_available:
@@ -137,7 +94,7 @@ if __name__ == '__main__':
 
         model = nn.DataParallel(model, device_ids=device_ids).cuda()
 
-    criterion = MultiLabelSoftMarginLoss()
+    criterion = CrossEntropyLoss()
 
     train_kwargs = dict(
         args=args,
@@ -146,7 +103,7 @@ if __name__ == '__main__':
         train_loader=train_loader,
         valid_loader=valid_loader,
         validation=validation,
-        patience=4,
+        patience=10,
     )
 
     if getattr(model, 'finetune', None):
@@ -162,18 +119,3 @@ if __name__ == '__main__':
         utils.train(
             init_optimizer=lambda lr: Adam(model.parameters(), lr=lr),
             **train_kwargs)
-
-
-    #
-    #
-    # utils.train(
-    #     init_optimizer=lambda lr: Adam(model.parameters(), lr=lr),
-    #     args=args,
-    #     model=model,
-    #     criterion=criterion,
-    #     train_loader=train_loader,
-    #     valid_loader=valid_loader,
-    #     validation=validation,
-    #     # save_predictions=save_predictions,
-    #     patience=2,
-    # )
